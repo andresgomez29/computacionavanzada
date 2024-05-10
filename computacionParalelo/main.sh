b845c3f61c6972911a97fa8db393e6301232de5a
Gustavo_Castrillon <gadolfo.castrillon@udea.edu.co>
2024-05-10 11:29:43 -0500

    Actualización main.sh

--- computacionParalelo/main.sh
+++ computacionParalelo/main.sh
@@ -1,10 +1,11 @@
 #script
 
-
-
 echo "Ejecutando script"
 
+mkdir salida 2>/dev/null
+
 echo "Generando datos"
+
 mpiexec -n 4 python paralell_main.py
 echo "Fin de la generación"
 
@@ -15,6 +16,6 @@ python graficador2.py
 
 echo "Graficado con exito"
 echo "Puedes ver las imagenes guardadas como:"
-echo "	-grafico1Paralelo.jpg"
-echo "	-grafico2Paralelo.jpg"
+echo "	-salida/grafico1Paralelo.jpg"
+echo "	-salida/grafico2Paralelo.jpg"
 echo "Finalizando programa"
